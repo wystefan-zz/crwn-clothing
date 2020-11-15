bb29de8d0bbb48f18b619601cbee38897338282b
Yang Wu <wuyangtju@gmail.com>
2020-11-14 18:23:17 -0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HnZpuIzQB7yDyhAKUUllODDzbA6zhz61tKNYUxnnSxEm1tT8WUpPeGqKpAh9uClXz06bCJOJtaW48ygv773LQpZ00gAgwBtfk';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { selectCartItemsTotal, selectCartItems} from '../../redux/cart/cart.sele
 import {createStructuredSelector} from 'reselect';
 import {connect} from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
@@ -28,9 +29,16 @@ const CheckoutPage = ({cartItems, total}) => (
             cartItems.map(cartItem =>
                 (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
         }
+        
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payment*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
